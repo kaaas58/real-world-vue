7147e3d35cc9f04e3a626cfd0fb32b735d95e702
Lukas Trapp <lukas.trapp@MacBookPro-LT.local>
2024-10-17 11:30:27 +0200

    test

---
+++ src/views/EventListView.vue
@@ -0,0 +1,54 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+
+const events = ref([
+  {
+  id: 5228101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organisazier: 'Kat Laydee'
+},
+{
+  id: 5948101,
+  category: 'animal welfare',
+  title: 'Communety Gardening',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organisazier: 'Kat Laydee'
+},
+{
+  id: 5928501,
+  category: 'animal welfare',
+  title: 'Beach cleanup',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organisazier: 'Kat Laydee'
+}])
+</script>
+
+<template>
+  <h1>Events for Good</h1>
+  <div class="events"> 
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<style scoped>
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
