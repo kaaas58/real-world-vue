57b04c6639c4ce2ecf4a3eb18c7cd18eaa77dc81
Lukas Trapp <lukas.trapp@MacBookPro-LT.local>
2024-10-17 12:55:23 +0200

    updating thru api

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,52 +1,31 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 
-const events = ref([
-  {
-  id: 5228101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organisazier: 'Kat Laydee'
-},
-{
-  id: 5948101,
-  category: 'animal welfare',
-  title: 'Communety Gardening',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organisazier: 'Kat Laydee'
-},
-{
-  id: 5928501,
-  category: 'animal welfare',
-  title: 'Beach cleanup',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organisazier: 'Kat Laydee'
-}])
+const events = ref(null)
+
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/Code-pop/Real-World_Vue-3/events')
+    .then(response => {
+      console.log('events:', response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 </script>
 
 <template>
   <h1>Events for Good</h1>
-  <div class="events"> 
-    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <style scoped>
-.events{
+.events {
   display: flex;
   flex-direction: column;
   align-items: center;
